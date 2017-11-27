d8bb6f494fb4ec75d67a0de1d7fb97f64f9baa5a
Max Lynch <max@drifty.com>
2017-11-27 14:08:51 -0800

    Leaving off

---
+++ packages/ios/Avocado/Avocado/Plugins/CameraPicker.swift
@@ -0,0 +1,49 @@
+import Foundation
+import CoreMotion
+
+@objc(CameraPicker)
+public class CameraPicker : Plugin,  {
+  @objc func open(_ call: PluginCall) {
+    if(bridge.isSimulator()) {
+      bridge.modulePrint(self, "Camera not available in simulator")
+      bridge.alert("Camera Error", "Camera not available in Simulator")
+      call.error("Camera not available while running in Simulator")
+    }
+    
+    if let missingUsageDescription = checkUsageDescriptions() {
+      bridge.modulePrint(self, missingUsageDescription)
+      call.error(missingUsageDescription)
+      bridge.alert("Camera Error", "Missing required usage description. See console for more information")
+      return
+    }
+    
+    let alert = UIAlertController(title: "Photo", message: nil, preferredStyle: UIAlertControllerStyle.actionSheet)
+    alert.addAction(UIAlertAction(title: "From Photos", style: .destructive, handler: { (action: UIAlertAction) in
+      self.bridge.reload()
+    }))
+    alert.addAction(UIAlertAction(title: "Take Picture", style: .destructive, handler: { (action: UIAlertAction) in
+      self.bridge.reload()
+    }))
+  }
+  
+  
+  /**
+   * Make sure the developer provided proper usage descriptions
+   * per apple's terms.
+   */
+  func checkUsageDescriptions() -> String? {
+    if let dict = Bundle.main.infoDictionary {
+      let hasPhotoLibraryUsage = dict["NSPhotoLibraryAddUsageDescription"] != nil
+      if !hasPhotoLibraryUsage {
+        let docLink = DocLinks.NSPhotoLibraryAddUsageDescription
+        return "You are missing NSPhotoLibraryAddUsageDescription in your Info.plist file." +
+        " Camera will not function without it. Learn more: \(docLink.rawValue)"
+      }
+    }
+    
+    return nil
+  }
+}
+
+
+

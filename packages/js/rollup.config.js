0ffdfbf9ea9fd6ee0e7b39beca6b0c637360edd1
Adam Bradley <adambradley25@gmail.com>
2017-11-29 21:31:23 -0600

    wip

---
+++ packages/js/rollup.config.js
@@ -0,0 +1,9 @@
+
+export default {
+  input: 'dist/esm/plugins/index.js',
+  output: {
+    file: 'dist/avocado.plugins.js',
+    format: 'iife',
+    name: 'avocadoPlugins'
+  }
+};
--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -13,16 +13,4 @@ export {
   Plugin, NativePlugin
 } from './plugin';
 
-export { Browser } from './plugins/browser';
-export { Camera } from './plugins/camera';
-export { Clipboard } from './plugins/clipboard';
-export { Device } from './plugins/device';
-export { Filesystem, FilesystemDirectory } from './plugins/fs';
-export { Geolocation } from './plugins/geolocation';
-export { Haptics, HapticsImpactStyle } from './plugins/haptics';
-export { LocalNotifications, LocalNotification, NotificationScheduleAt } from './plugins/local-notifications';
-export { Modals } from './plugins/modals';
-export { Motion } from './plugins/motion';
-export { Network } from './plugins/network';
-export { SplashScreen } from './plugins/splashscreen';
-export { StatusBar, StatusBarStyle } from './plugins/statusbar';
+export * from './plugins/index';

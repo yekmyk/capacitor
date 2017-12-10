dc2b4cc85a15dceded63ea88f583bdba987f543b
Max Lynch <max@drifty.com>
2017-12-10 16:49:01 -0600

    JS_METHOD

---
+++ ios/Avocado/Avocado/JSExport.h
@@ -0,0 +1,13 @@
+//
+//  JSExport.h
+//  Avocado
+//
+//  Created by Max Lynch on 12/10/17.
+//  Copyright © 2017 Drifty Co. All rights reserved.
+//
+
+#ifndef JSExport_h
+#define JSExport_h
+
+
+#endif /* JSExport_h */
---
+++ ios/Avocado/Avocado/JSExport.m
@@ -0,0 +1,9 @@
+//
+//  JSExport.m
+//  Avocado
+//
+//  Created by Max Lynch on 12/10/17.
+//  Copyright © 2017 Drifty Co. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>

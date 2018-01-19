8bad09f67ac030e171f2c6cff3b55e7f44393cfd
Max Lynch <max@drifty.com>
2018-01-18 22:41:05 -0600

    Fix social icons. Closes #72

---
+++ build/app/sqi87dqk.js
@@ -0,0 +1,4 @@
+/*! Built with http://stenciljs.com */
+
+App.loadStyles("site-bar","site-bar{display:block;width:100%;padding:10px;background-color:red;background-color:#f54f4f;text-align:center;color:#fff;font-size:13px}site-bar a{color:#000;font-weight:500}\nsite-bar.hydrated{visibility:inherit}");
+App.loadComponents("sqi87dqk",function(e,r,t,n){"use strict";class i{render(){return r("div",{class:"site-bar"},"Capacitor is in Private Preview. Read the ",r("stencil-route-link",{url:"/blog"},"announcement blog")," for more info on the project")}}e["site-bar"]=i},["site-bar",0,{}]);;
--- sw.js
+++ sw.js
@@ -104,23 +104,19 @@ const fileManifest = [
   },
   {
     "url": "build/app.js",
-    "revision": "285b98925bcb419af12e99644874a265"
+    "revision": "99c3aeaa0249c264f188b489dfdd3507"
   },
   {
-    "url": "build/app/6bo0uds8.js",
-    "revision": "30baafc40594eb4364f17c106bc72010"
+    "url": "build/app/3mk3rmrr.js",
+    "revision": "ae36f081da4e32bbe2d3fb23fb0e11ea"
+  },
+  {
+    "url": "build/app/4aobwvoq.js",
+    "revision": "ae87c55b6892652e607e2e3f282187d0"
   },
   {
     "url": "build/app/9vp1kpca.js",
     "revision": "ec63c784fbc2f0b40f0bcdb8ce78ee07"
-  },
-  {
-    "url": "build/app/a67u4vho.js",
-    "revision": "a5448d101432d5f6c9696b8f017c91cc"
-  },
-  {
-    "url": "build/app/a9pov8p9.js",
-    "revision": "396bc3afa4b6aabbea62862f6283645e"
   },
   {
     "url": "build/app/app.global.js",
@@ -140,23 +136,31 @@ const fileManifest = [
   },
   {
     "url": "build/app/app.registry.json",
-    "revision": "b17b083d7f8e16652bc85b22c13eb5da"
+    "revision": "9d3303603ea2cf9f3b52b99946d3a3cc"
   },
   {
     "url": "build/app/cv7cbtbi.js",
     "revision": "b425d7fba0b1c25730cf13a34af6b556"
   },
   {
+    "url": "build/app/defza7vd.js",
+    "revision": "ab9c98a8cbc1aca477808e063455d952"
+  },
+  {
+    "url": "build/app/ibgx2z2t.js",
+    "revision": "e22fc160169f3949d0cb8dcde34b45fd"
+  },
+  {
     "url": "build/app/krirex7d.js",
     "revision": "2379293fa04a47b457246d63856c5362"
   },
   {
-    "url": "build/app/lljbbqab.js",
-    "revision": "4cb1be69cd6f9e9015e3d5d93d0159e5"
+    "url": "build/app/lfeghk8g.js",
+    "revision": "eb1f8d600a6c6c0c40e837d490e58a59"
   },
   {
-    "url": "build/app/lppew0yu.js",
-    "revision": "6e6e62ef569bd610b181f19a188a47f8"
+    "url": "build/app/lljbbqab.js",
+    "revision": "4cb1be69cd6f9e9015e3d5d93d0159e5"
   },
   {
     "url": "build/app/n56a2jzp.js",
@@ -175,252 +179,108 @@ const fileManifest = [
     "revision": "bceea4fa858c798bb6fa201033865b51"
   },
   {
-    "url": "build/app/px2eltlm.js",
-    "revision": "cf509fdf6798f9806d2db1cbc74c56a0"
-  },
-  {
-    "url": "build/app/pxqetkvb.js",
-    "revision": "b265badb0f3b157462d6173e37e36a30"
-  },
-  {
     "url": "build/app/rhlmfaus.js",
     "revision": "6be3c0d5f97086f8fab64b1bf2a654a5"
   },
   {
-    "url": "build/app/rjkvhcbv.js",
-    "revision": "48c55f83fea984765543325f8a71dd02"
+    "url": "build/app/sqi87dqk.js",
+    "revision": "2c546dbf30d539745ad397d4f54fd5c0"
   },
   {
-    "url": "build/app/sw8p3ksn.js",
-    "revision": "8dc0eab6b1f0fe6587f9a456b07cd6b8"
+    "url": "build/app/waljrbe1.js",
+    "revision": "68e972c9289dffeceb9a02d63e2da8c6"
+  },
+  {
+    "url": "build/app/wnmbbgpp.js",
+    "revision": "382d6822942bd45881e9b3874b5ed057"
   },
   {
     "url": "build/app/xyfissap.js",
     "revision": "25d03519f99399851b14fdc57d211da6"
   },
   {
-    "url": "docs-content/addons/stencil-router.html",
-    "revision": "c2790bae4e03529c153d19daa0486f39"
-  },
-  {
-    "url": "docs-content/advanced/angular-integration/index.html",
-    "revision": "7bd66f3ef35755f6729e061794a99e5e"
-  },
-  {
-    "url": "docs-content/advanced/distribution/index.html",
-    "revision": "80003ea184cb027dc421b157ba495dc3"
-  },
-  {
-    "url": "docs-content/advanced/pre-rendering/index.html",
-    "revision": "f0f447abe7f473d7eee5b948c7a383e8"
-  },
-  {
-    "url": "docs-content/advanced/service-worker/index.html",
-    "revision": "d46601d6e44303a9e5f316427f693595"
-  },
-  {
-    "url": "docs-content/advanced/shadow-dom/index.html",
-    "revision": "f6b45047ffd1103af8a6c3c50393843c"
-  },
-  {
-    "url": "docs-content/advanced/ssr/index.html",
-    "revision": "c5696040315280af27a3f40a09541dba"
-  },
-  {
     "url": "docs-content/android/configuration.html",
-    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+    "revision": "e2c8ddd3ca278c0ffc38697e75bb4462"
+  },
+  {
+    "url": "docs-content/android/index.html",
+    "revision": "d6bba3435fe3f09eb1d971b14d1f76e1"
+  },
+  {
+    "url": "docs-content/android/lifecycle.html",
+    "revision": "f46c3f0b3d79a61156591c6df5361fa5"
   },
   {
     "url": "docs-content/android/managing-dependencies.html",
+    "revision": "bd95e9c918adc906b5d338e1b3362518"
+  },
+  {
+    "url": "docs-content/android/plugins.html",
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   },
   {
-    "url": "docs-content/apis/accessibility/api-index.html",
-    "revision": "8a1452ff00133ab6f0628d315a04daeb"
-  },
-  {
-    "url": "docs-content/apis/accessibility/api.html",
-    "revision": "72ef3e7ee3dec38ab075f1db2d52d4f2"
-  },
-  {
     "url": "docs-content/apis/accessibility/index.html",
-    "revision": "04a1f73ccead27ed3ada6b5a66422afa"
-  },
-  {
-    "url": "docs-content/apis/action-sheet/api.html",
-    "revision": "3ffe2ffa8453869d498ac2e6471a7e82"
+    "revision": "03404f657fa97e28f9d35cc5003a5b30"
   },
   {
     "url": "docs-content/apis/action-sheet/index.html",
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   },
   {
-    "url": "docs-content/apis/app-state/api-index.html",
-    "revision": "e2207ab59aa67612832360a65089826a"
-  },
-  {
-    "url": "docs-content/apis/app-state/api.html",
-    "revision": "446e4558627e59839f189245fbbfe2ed"
-  },
-  {
     "url": "docs-content/apis/app-state/index.html",
-    "revision": "e212f8f534e9c3dc74f1cc3c355ff2f5"
-  },
-  {
-    "url": "docs-content/apis/browser/api-index.html",
-    "revision": "1f25cccc29c0abb5e334aaa8d262c9d4"
-  },
-  {
-    "url": "docs-content/apis/browser/api.html",
-    "revision": "7113b26122766a54d6af4832fe726d60"
+    "revision": "b15acb1e6d1ee4a0773404ddef84c006"
   },
   {
     "url": "docs-content/apis/browser/index.html",
-    "revision": "cbcdd7df580bb8c4b1c54ba83a440394"
-  },
-  {
-    "url": "docs-content/apis/camera/api-index.html",
-    "revision": "8fe1f04dec60661b22ba77c4c0e131f4"
-  },
-  {
-    "url": "docs-content/apis/camera/api.html",
-    "revision": "e5936ac3020c8a273e5e5d1bd233e6c2"
+    "revision": "a6f1739ade4f5eeeb39031d6b4bd9a5b"
   },
   {
     "url": "docs-content/apis/camera/index.html",
-    "revision": "9dee3fa8dc87a090d449dd9d679469bf"
-  },
-  {
-    "url": "docs-content/apis/clipboard/api-index.html",
-    "revision": "35e61c8fe3baa6405d57f7a4bab70707"
-  },
-  {
-    "url": "docs-content/apis/clipboard/api.html",
-    "revision": "e777f5a3f644181bdefa2c9df1cb8dd9"
+    "revision": "cf411d7a4664f6bf17ce703681bcac05"
   },
   {
     "url": "docs-content/apis/clipboard/index.html",
-    "revision": "0da6f103690a8ce2fbd3118b2113a662"
+    "revision": "f7fbf63ccb1d0237767e4666f71a22b0"
   },
   {
     "url": "docs-content/apis/console/index.html",
-    "revision": "ef48fee27581a2dfc7b0b83fc4b8fd32"
-  },
-  {
-    "url": "docs-content/apis/device/api-index.html",
-    "revision": "b7ea5710eba20e4ef0a00907c82e324e"
-  },
-  {
-    "url": "docs-content/apis/device/api.html",
-    "revision": "b317f08f62374a3208527963771a3b80"
+    "revision": "8a3f3e9744935bf0330f458e3ec82922"
   },
   {
     "url": "docs-content/apis/device/index.html",
     "revision": "50cf608c273c02967f46d7e40f7fd877"
   },
   {
-    "url": "docs-content/apis/filesystem/api-index.html",
-    "revision": "8940da970913f0089dd37775123d687c"
-  },
-  {
-    "url": "docs-content/apis/filesystem/api.html",
-    "revision": "1af06ba80c64f4cf2d4d993a9585009c"
-  },
-  {
     "url": "docs-content/apis/filesystem/index.html",
-    "revision": "9f9306841c9f611cd3350c845e7bfc7c"
-  },
-  {
-    "url": "docs-content/apis/geolocation/api-index.html",
-    "revision": "bbe9c502412b099b853db9eb0af074a1"
-  },
-  {
-    "url": "docs-content/apis/geolocation/api.html",
-    "revision": "8b8071f292df7ddf23cb173fd256405c"
+    "revision": "92ba04ea915c3bb271aaa228364755e6"
   },
   {
     "url": "docs-content/apis/geolocation/index.html",
     "revision": "5ecc744029872037d775bf52e58715ba"
   },
   {
-    "url": "docs-content/apis/haptics/api-index.html",
-    "revision": "720dd0704a6f502f85a3a411ee487672"
-  },
-  {
-    "url": "docs-content/apis/haptics/api.html",
-    "revision": "3fb144695e73850ccef007514fb4eba8"
-  },
-  {
     "url": "docs-content/apis/haptics/index.html",
-    "revision": "ff4beebfdb33d4cf02b57806da6b8afc"
-  },
-  {
-    "url": "docs-content/apis/keyboard/api-index.html",
-    "revision": "e050c20cdde57c46f4f9c0191019c441"
-  },
-  {
-    "url": "docs-content/apis/keyboard/api.html",
-    "revision": "d1018c157735223acd96e7fabc2d62bb"
+    "revision": "b130307831d8bb93080d48cc8987d905"
   },
   {
     "url": "docs-content/apis/keyboard/index.html",
     "revision": "c552be552a02e2b27acb97aa46e658c7"
   },
   {
-    "url": "docs-content/apis/local-notifications/api-index.html",
-    "revision": "e6dee56dbf1839ed2ae4c0c2c01e4432"
-  },
-  {
-    "url": "docs-content/apis/local-notifications/api.html",
-    "revision": "c6f44a02abbec4e362c7dd7a1e78ca03"
-  },
-  {
     "url": "docs-content/apis/local-notifications/index.html",
     "revision": "a2daa6f2c0802d045cb0c407b53b34c4"
   },
   {
-    "url": "docs-content/apis/modals/api-index.html",
-    "revision": "e47b551faef400812b94f42858323a27"
-  },
-  {
-    "url": "docs-content/apis/modals/api.html",
-    "revision": "cf645c1a7cc6551bb70a503e6a03ff16"
-  },
-  {
     "url": "docs-content/apis/modals/index.html",
-    "revision": "9a2c532e3155e25b999c00d82d241bc8"
-  },
-  {
-    "url": "docs-content/apis/motion/api-index.html",
-    "revision": "e2207ab59aa67612832360a65089826a"
-  },
-  {
-    "url": "docs-content/apis/motion/api.html",
-    "revision": "7694cf1dc6a198a2360d55b9e1240ba7"
+    "revision": "51fe492510fec2bbe52c35220f9f4287"
   },
   {
     "url": "docs-content/apis/motion/index.html",
     "revision": "0e52bbf40f136a2515bbb983b3f04be2"
   },
   {
-    "url": "docs-content/apis/network/api-index.html",
-    "revision": "c60515a73e88c5e49667f8fe4712025a"
-  },
-  {
-    "url": "docs-content/apis/network/api.html",
-    "revision": "9210a0f30533b66cd1f0fc2655a835cf"
-  },
-  {
     "url": "docs-content/apis/network/index.html",
-    "revision": "ccf6ff184816c6b202869a753c6b5950"
-  },
-  {
-    "url": "docs-content/apis/photos/api-index.html",
-    "revision": "b24478efa3459ba287906cc6f534f763"
-  },
-  {
-    "url": "docs-content/apis/photos/api.html",
-    "revision": "3de351525559f6e2cdba84afb5e7d074"
+    "revision": "1291d958cffd84a12750f6eae7285767"
   },
   {
     "url": "docs-content/apis/photos/index.html",
@@ -435,28 +295,12 @@ const fileManifest = [
     "revision": "9dbf15fb5daddabef501c83aed2a68e4"
   },
   {
-    "url": "docs-content/apis/splash-screen/api-index.html",
-    "revision": "ece560175f6213b80afd4f1dcdec26bd"
-  },
-  {
-    "url": "docs-content/apis/splash-screen/api.html",
-    "revision": "c84d959b3e27789ab9c07fa95f459b71"
-  },
-  {
     "url": "docs-content/apis/splash-screen/index.html",
     "revision": "4cb2716767134c10b9cc1a8dbbb4a0ab"
   },
   {
-    "url": "docs-content/apis/status-bar/api-index.html",
-    "revision": "928555f73f73a6df81d13b670145d85c"
-  },
-  {
-    "url": "docs-content/apis/status-bar/api.html",
-    "revision": "467b98e507c06b2b872d3b2817bf8aa2"
-  },
-  {
     "url": "docs-content/apis/status-bar/index.html",
-    "revision": "8eca2f9ba2e59f1d99b15d010d3161d3"
+    "revision": "598cfd7c9a8ca44cdafa7b7ca48c205e"
   },
   {
     "url": "docs-content/apis/storage/index.html",
@@ -468,123 +312,83 @@ const fileManifest = [
   },
   {
     "url": "docs-content/basics/app-project-structure.html",
-    "revision": "d7f5fbded8eeca7fc7397ec496f0108d"
+    "revision": "e45d913f2568d9d7567107f56b195f77"
   },
   {
     "url": "docs-content/basics/building-your-app.html",
-    "revision": "d9d82945a76f22ab1ff93a67fcf8a259"
-  },
-  {
-    "url": "docs-content/basics/component-lifecycle.html",
-    "revision": "f94b50ebb42950bfc71336b61abc2b40"
+    "revision": "25a03d48830c48daf38719841e41e14c"
   },
   {
     "url": "docs-content/basics/configuring-your-app.html",
-    "revision": "ff88901bcbddffdba9e888801ab0f552"
+    "revision": "140de1ac0e0e4268ef803cb4333d1547"
   },
   {
     "url": "docs-content/basics/creating-apps.html",
-    "revision": "578b7e61f8128e22e48f1b8b2df9ecc3"
-  },
-  {
-    "url": "docs-content/basics/decorators.html",
-    "revision": "1192a18adfaf9f024e158a253de6ccec"
-  },
-  {
-    "url": "docs-content/basics/events.html",
-    "revision": "daa0ca09ba0388faf45ca50acdeb64f1"
-  },
-  {
-    "url": "docs-content/basics/forms.html",
-    "revision": "e645d102707c5d31e3ca463400ab7bd2"
-  },
-  {
-    "url": "docs-content/basics/handling-arrays.html",
-    "revision": "e3db7a344eff3b92b1bd1fa798d94599"
+    "revision": "c0ff30be196e4b040d076fcf93c2d899"
   },
   {
     "url": "docs-content/basics/managing-platforms.html",
-    "revision": "6a98177cdbea8085d7a925e04ee3a8d5"
-  },
-  {
-    "url": "docs-content/basics/my-first-component.html",
-    "revision": "5bf814be1ae69cf68188f4e1253edc34"
+    "revision": "2d32a3349d035db2bda5ffdd14f165da"
   },
   {
     "url": "docs-content/basics/running-your-app.html",
-    "revision": "d3367b599e5ea91c51cb42c795cf1a28"
+    "revision": "15cd91bef2fbddb9cf1714446e0ce4c5"
   },
   {
-    "url": "docs-content/basics/stencil-config.html",
-    "revision": "9f2483724de100afaa8d972409f1f438"
+    "url": "docs-content/getting-started/index.html",
+    "revision": "fb9bb7c1d938973b6026a7393ac4aaf6"
   },
   {
-    "url": "docs-content/basics/templating.html",
-    "revision": "e58f462a976bc5dc581f221273c66280"
+    "url": "docs-content/getting-started/migrating-from-phonegap-cordova.html",
+    "revision": "e10d097adf02e9a8cc22a95735c757de"
   },
   {
-    "url": "docs-content/basics/testing.html",
-    "revision": "26a89a2ffa18b6278c60225aec05635e"
-  },
-  {
-    "url": "docs-content/compiler/config.html",
-    "revision": "f9c2cda306801bb1b38562175b7719d6"
-  },
-  {
-    "url": "docs-content/intro/browsers.html",
-    "revision": "d4fd210849e901aa9ddc213d72b8b063"
-  },
-  {
-    "url": "docs-content/intro/getting-started.html",
-    "revision": "59945f36fb42bbacb1eb57e714d3cd1b"
-  },
-  {
-    "url": "docs-content/intro/index.html",
-    "revision": "01c0c5b5af09fc640efb14aaeec7914f"
-  },
-  {
-    "url": "docs-content/intro/migrating-from-phonegap-cordova.html",
-    "revision": "97c374c14c7abd1c7cf8edeb2dee9f71"
+    "url": "docs-content/index.html",
+    "revision": "e82ba712ccfa509d27553e6652c15685"
   },
   {
     "url": "docs-content/ios/configuration.html",
-    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+    "revision": "da0bb7131a24082845c72e3fbdb98d08"
+  },
+  {
+    "url": "docs-content/ios/index.html",
+    "revision": "8a0a158c7e7d68c9b3baf6f69247270b"
   },
   {
     "url": "docs-content/ios/managing-dependencies.html",
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   },
   {
+    "url": "docs-content/ios/plugins.html",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  },
+  {
     "url": "docs-content/plugins/creating-plugins.html",
-    "revision": "e0baf1a49824a26d4bc5e4e369b41e1d"
+    "revision": "59708d86331d45eaf22055113f1b779e"
   },
   {
     "url": "docs-content/plugins/index.html",
-    "revision": "7f3e45e34d816318293edd4b8802b5dc"
+    "revision": "2b8b84d45c12c7713dd6dcbd26d45dc8"
   },
   {
     "url": "docs-content/plugins/plugin-api-javascript.html",
-    "revision": "39b5d2445d5998a98788aa0822a52f4f"
-  },
-  {
-    "url": "docs-content/start/index.html",
-    "revision": "2aabcb99454b00e03c9ec799255c63a0"
+    "revision": "1942b612aa992b8830c5bcab51f2c20b"
   },
   {
     "url": "docs/intro/index.html",
-    "revision": "b3533ace9c1d72535785ea71aa3c9660"
+    "revision": "2db3e8d0408dba94808e9b87bce7cbdc"
   },
   {
     "url": "host.config.json",
-    "revision": "c1d97d68d9a9acd44e90bc0604425e2e"
+    "revision": "e538444b8bfbee8d92273260c0824bfe"
   },
   {
     "url": "index.html",
-    "revision": "97e8d2f96e606f1f81d9d24b6c48037d"
+    "revision": "d6a8841d6dd9f181bce447c91ce5f7bf"
   },
   {
     "url": "manifest.json",
-    "revision": "10fddfb1d610b09dfc1c4578e1f2faf2"
+    "revision": "933334db099a12ff06af4b62e3c30872"
   }
 ];
 

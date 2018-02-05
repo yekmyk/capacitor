1d0f635a51d32d7b883e67439991131bdabd0b0b
Max Lynch <max@drifty.com>
2018-02-04 19:55:22 -0600

    Remove old files

--- build/app/4aobwvoq.js
+++
@@ -1,4 +0,0 @@
-/*! Built with http://stenciljs.com (es5) */
-
-App.loadStyles("site-bar","site-bar{display:block;width:100%;padding:10px;background-color:red;background-color:#f54f4f;text-align:center;color:#fff;font-size:13px}site-bar a{color:#000;font-weight:500}\nsite-bar.hydrated{visibility:inherit}");
-App.loadComponents("4aobwvoq",function(e,n,t,r){"use strict";var o=function(){function e(){}return e.prototype.render=function(){return n("div",{class:"site-bar"},"Capacitor is in Private Preview. Read the ",n("stencil-route-link",{url:"/blog"},"announcement blog")," for more info on the project")},e}();e["site-bar"]=o},["site-bar",0,{}]);;
